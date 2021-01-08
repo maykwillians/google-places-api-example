c17870be6d88ecaf181e82072ad40c2dc2de0e70
Mayk Menezes <maykwillians@gmail.com>
2021-01-08 09:21:24 -0300

    Refactoring

--- app/src/main/java/com/maykmenezes/googleplacesapiexample/model/PlacesVO.kt
+++ app/src/main/java/com/maykmenezes/googleplacesapiexample/model/PlacesVO.kt
@@ -8,46 +8,24 @@ data class PlacesVO(
 )
 
 data class ResultsItem(
-
-		@field:SerializedName("reference")
-		val reference: String? = null,
-
 		@field:SerializedName("types")
 		val types: List<String?>? = null,
-
-		@field:SerializedName("icon")
-		val icon: String? = null,
-
 		@field:SerializedName("name")
 		val name: String? = null,
-
 		@field:SerializedName("rating")
 		val rating: String? = null,
-
 		@field:SerializedName("geometry")
 		val geometry: Geometry? = null,
-
-		@field:SerializedName("vicinity")
-		val vicinity: String? = null,
-
-		@field:SerializedName("id")
-		val id: String? = null,
-
-		@field:SerializedName("place_id")
-		val placeId: String? = null
 )
 
 data class Geometry(
-
 		@field:SerializedName("location")
 		val location: Location? = null
 )
 
 data class Location(
-
 		@field:SerializedName("lng")
 		val lng: Double? = null,
-
 		@field:SerializedName("lat")
 		val lat: Double? = null
 )
